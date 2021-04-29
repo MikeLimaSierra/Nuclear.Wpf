07c5cb612687b74217c1b9c45b89186e1e25b75f
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-04-29 23:04:06 +0200

    add data extensions

---
+++ src/Nuclear.Wpf.Data/DataExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Nuclear.Wpf.Data {
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined object inside XAML markup.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    public class DataExtension<T> : MarkupExtension {
+
+        #region properties
+
+        /// <summary>
+        /// Gets the declared object that will be provided.
+        /// </summary>
+        public T Value { get; }
+
+        #endregion
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="DataExtension{T}"/>.
+        /// </summary>
+        /// <param name="value">The value that will be provided.</param>
+        public DataExtension(T value) {
+            Value = value;
+        }
+
+        #endregion
+
+        #region method
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        public override Object ProvideValue(IServiceProvider serviceProvider) => Value;
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+
+        #endregion
+
+    }
+
+}
---
+++ src/Nuclear.Wpf.Data/DataExtension.generic.cs
@@ -0,0 +1,209 @@
+﻿using System;
+
+namespace Nuclear.Wpf.Data {
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="Boolean"/> inside XAML markup.
+    /// </summary>
+    public class BooleanExtension : DataExtension<Boolean> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="BooleanExtension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="Boolean"/> that will be provided.</param>
+        public BooleanExtension(Boolean value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="Byte"/> inside XAML markup.
+    /// </summary>
+    public class ByteExtension : DataExtension<Byte> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="ByteExtension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="Byte"/> that will be provided.</param>
+        public ByteExtension(Byte value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="SByte"/> inside XAML markup.
+    /// </summary>
+    public class SByteExtension : DataExtension<SByte> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="SByteExtension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="SByte"/> that will be provided.</param>
+        public SByteExtension(SByte value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="Int16"/> inside XAML markup.
+    /// </summary>
+    public class Int16Extension : DataExtension<Int16> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Int16Extension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="Int16"/> that will be provided.</param>
+        public Int16Extension(Int16 value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="Int32"/> inside XAML markup.
+    /// </summary>
+    public class Int32Extension : DataExtension<Int32> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Int32Extension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="Int32"/> that will be provided.</param>
+        public Int32Extension(Int32 value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="Int64"/> inside XAML markup.
+    /// </summary>
+    public class Int64Extension : DataExtension<Int64> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Int64Extension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="Int64"/> that will be provided.</param>
+        public Int64Extension(Int64 value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="UInt16"/> inside XAML markup.
+    /// </summary>
+    public class UInt16Extension : DataExtension<UInt16> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="UInt16Extension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="UInt16"/> that will be provided.</param>
+        public UInt16Extension(UInt16 value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="UInt32"/> inside XAML markup.
+    /// </summary>
+    public class UInt32Extension : DataExtension<UInt32> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="UInt32Extension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="UInt32"/> that will be provided.</param>
+        public UInt32Extension(UInt32 value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="UInt64"/> inside XAML markup.
+    /// </summary>
+    public class UInt64Extension : DataExtension<UInt64> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="UInt64Extension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="UInt64"/> that will be provided.</param>
+        public UInt64Extension(UInt64 value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="Single"/> inside XAML markup.
+    /// </summary>
+    public class SingleExtensions : DataExtension<Single> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="SingleExtensions"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="Single"/> that will be provided.</param>
+        public SingleExtensions(Single value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="Double"/> inside XAML markup.
+    /// </summary>
+    public class DoubleExtension : DataExtension<Double> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="DoubleExtension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="Double"/> that will be provided.</param>
+        public DoubleExtension(Double value) : base(value) { }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Implements a markup extension to declare an inlined <see cref="Decimal"/> inside XAML markup.
+    /// </summary>
+    public class DecimalExtension : DataExtension<Decimal> {
+
+        #region ctors
+
+        /// <summary>
+        /// Creates a new instance of <see cref="DecimalExtension"/>.
+        /// </summary>
+        /// <param name="value">The <see cref="Decimal"/> that will be provided.</param>
+        public DecimalExtension(Decimal value) : base(value) { }
+
+        #endregion
+
+    }
+
+}
