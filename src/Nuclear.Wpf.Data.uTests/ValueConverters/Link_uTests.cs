22dbc03ea26b74dbf55b62ed72cb2f870f0ca499
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-04-30 22:29:22 +0200

    link

---
+++ src/Nuclear.Wpf.Data.uTests/ValueConverters/Link_uTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+using Nuclear.TestSite;
+
+namespace Nuclear.Wpf.Data.ValueConverters {
+    class Link_uTests {
+
+        #region properties
+
+        [TestMethod]
+        [TestData(nameof(ConverterData))]
+        void Converter(IValueConverter converter) {
+
+            Link link = new Link();
+
+            Test.IfNot.Action.ThrowsException(() => link.Converter = converter, out Exception ex);
+
+            Test.If.Reference.IsEqual(link.Converter, converter);
+
+        }
+
+        IEnumerable<Object[]> ConverterData() {
+            return new List<Object[]>() {
+                new Object[] { null },
+                new Object[] { new InvertedBooleanConverter() },
+                new Object[] { new NullToColorConverter() },
+            };
+        }
+
+        [TestMethod]
+        [TestData(nameof(ParameterData))]
+        void Parameter(Object parameter) {
+
+            Link link = new Link();
+
+            Test.IfNot.Action.ThrowsException(() => link.Parameter = parameter, out Exception ex);
+
+            Test.If.Reference.IsEqual(link.Parameter, parameter);
+
+        }
+
+        IEnumerable<Object[]> ParameterData() {
+            return new List<Object[]>() {
+                new Object[] { null },
+                new Object[] { "" },
+                new Object[] { " " },
+                new Object[] { "true" },
+                new Object[] { 1 },
+                new Object[] { new Object() },
+            };
+        }
+
+        [TestMethod]
+        [TestData(nameof(CultureData))]
+        void Culture(CultureInfo culture) {
+
+            Link link = new Link();
+
+            Test.IfNot.Action.ThrowsException(() => link.Culture = culture, out Exception ex);
+
+            Test.If.Reference.IsEqual(link.Culture, culture);
+
+        }
+
+        IEnumerable<Object[]> CultureData() {
+            return new List<Object[]>() {
+                new Object[] { null },
+                new Object[] { CultureInfo.InvariantCulture },
+                new Object[] { CultureInfo.CurrentCulture },
+                new Object[] { CultureInfo.GetCultureInfo("en-us") },
+                new Object[] { CultureInfo.GetCultureInfo("de-de") },
+            };
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/Nuclear.Wpf.Data/ValueConverters/Link.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Nuclear.Wpf.Data.ValueConverters {
+
+    /// <summary>
+    /// Implementation of a conversion link that is used in a conversion <see cref="Chain"/>.
+    /// </summary>
+    public class Link : MarkupExtension {
+
+        #region properties
+
+        /// <summary>
+        /// Gets or sets the <see cref="IValueConverter"/> instance.
+        /// </summary>
+        public IValueConverter Converter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parameter for the conversion.
+        /// </summary>
+        public Object Parameter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the used <see cref="CultureInfo"/> for the conversion.
+        /// </summary>
+        public CultureInfo Culture { get; set; }
+
+        #endregion
+
+        #region methods
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        public override Object ProvideValue(IServiceProvider serviceProvider) => this;
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+
+        #endregion
+
+    }
+}
