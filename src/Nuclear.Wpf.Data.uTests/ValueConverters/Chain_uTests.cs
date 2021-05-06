530623d1da5a87304e96412f7b46b740530de2d9
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-05-06 22:53:12 +0200

    Update Chain_uTests.cs

--- src/Nuclear.Wpf.Data.uTests/ValueConverters/Chain_uTests.cs
+++ src/Nuclear.Wpf.Data.uTests/ValueConverters/Chain_uTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Media;
 
 using Nuclear.TestSite;
 using Nuclear.Wpf.Data.ValueConverters.Base;
@@ -13,6 +15,8 @@ namespace Nuclear.Wpf.Data.ValueConverters {
             Test.If.Type.IsSubClass<Chain, BaseValueConverter>();
 
         }
+
+        #region ctors
 
         [TestMethod]
         void Ctor_Params() {
@@ -81,6 +85,35 @@ namespace Nuclear.Wpf.Data.ValueConverters {
             Test.If.Enumerable.Matches(chain.Links, new Link[] { l1, l2, l3, l4, l5, l6, l7, l8 });
 
         }
+
+        #endregion
+
+        #region Convert
+
+        [TestMethod]
+        [TestData(nameof(ConvertData))]
+        void Convert(Object input, Object expected, Link[] links) {
+
+            Chain chain = new Chain(links);
+            Object result = default;
+
+            Test.IfNot.Action.ThrowsException(() => result = chain.Convert(input, default, default, default), out Exception ex);
+
+            Test.If.Value.IsEqual(result, expected);
+
+        }
+
+        IEnumerable<Object[]> ConvertData() {
+            Link l1 = new Link() { Converter = new BooleanToStringConverter() };
+            Link l2 = new Link() { Converter = new StringToColorConverter() };
+
+            return new List<Object[]>() {
+                new Object[] { true, "true", new Link[] { l1 } },
+                new Object[] { true, Colors.Green, new Link[] { l1, l2 } },
+            };
+        }
+
+        #endregion
 
     }
 }
