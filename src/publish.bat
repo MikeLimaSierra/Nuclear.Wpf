1dcd8152f12908ad99c2c274a086f54d3e7c77f8
MikeLimaSierra <31997758+MikeLimaSierra@users.noreply.github.com>
2021-05-20 15:28:34 +0200

    reduce publishing to single generic script

--- src/publish.bat
+++ src/publish.bat
@@ -1,14 +1,24 @@
 @ECHO OFF
 
+CD /D "%~dp0"
+
 SET sn_exe="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe"
-SET key=..\KeyPair.snk
+SET key=KeyPair.snk
 SET tfm=%1
-SET bin=..\..\bin\Nuclear.Wpf.Data\AnyCPU\Release\%tfm%\
-SET publish=..\..\publish\Nuclear.Wpf.Data\%tfm%\
-SET dll=Nuclear.Wpf.Data.dll
-SET pdb=Nuclear.Wpf.Data.pdb
-SET xml=Nuclear.Wpf.Data.xml
-SET deps=Nuclear.Wpf.Data.deps.json
+SET ns=%2
+SET asm=%3
+
+SET bin=..\bin\%ns%\AnyCPU\Release\%tfm%\
+SET publish=..\publish\%ns%\%tfm%\
+SET dll=%asm%.dll
+SET pdb=%asm%.pdb
+SET xml=%asm%.xml
+SET deps=%asm%.deps.json
+
+ECHO "### Publishing ###"
+ECHO "Assembly: %asm%.dll"
+ECHO "Bin: %bin%"
+ECHO "Pub: %publish%"
 
 REM delete publish dir
 RMDIR /S /Q %publish%
